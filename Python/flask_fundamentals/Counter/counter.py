a1299b8e1d167582868e8f940cbd76922037a64c
Matthew Ambrosecchio <808.ambrosmt@gmail.com>
2018-01-16 16:44:27 -0800

    first Django application

--- Python/flask_fundamentals/Counter/counter.py
+++ Python/flask_fundamentals/Counter/counter.py
@@ -21,3 +21,31 @@ def multiple(amount):
     return index(int(amount))
 
 app.run(debug=True)
+
+'''
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = ":)"
+
+
+@app.route('/')
+def index(amount = 0):
+    if "count" in session:
+        session["count"] += amount
+    else:
+        session["count"] = 0
+
+    return render_template('index.html')
+
+@app.route('/<amount>')#set route to whatever amount we set
+def multiple(amount):
+    return index(int(amount))
+
+@app.route('/reset')
+def reset():
+    session["count"] = 1
+    return index(0)
+
+app.run(debug=True)
+'''
