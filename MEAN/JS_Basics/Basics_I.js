dfcfdccf43d65336c0079893d89cc96d0348ad7f
Matthew Ambrosecchio <808.ambrosmt@gmail.com>
2018-03-05 08:12:14 -0800

    First assignment for MEAN stack

---
+++ MEAN/JS_Basics/Basics_I.js
@@ -0,0 +1,27 @@
+// var x = [];
+// x.push("Coding", "Dojo", "Rocks");
+// x.pop();
+// console.log(x);
+
+// const y = [];
+// y.push(88);
+// console.log(y);
+
+// var z = [9,10,6,5,-1,20,13,2];
+// for(i = 0; i < z.length; i++){
+// for(i = 0; i < z.length-1; i++){
+//     console.log(z[i])
+// }
+
+// var names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
+// for(i=0; i < names.length; i++){
+//     // console.log(names[i].length);
+//     if(names[i].length == 5)
+//     console.log(names[i])
+// }
+
+function yell(string){
+    console.log(string.toUpperCase()+"!!!")
+}
+
+yell("Hello");
